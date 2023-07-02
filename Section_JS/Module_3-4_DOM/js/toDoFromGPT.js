b8b91cdf6ffc44411ce318b099803edb72f973b2
unknown <miamby111@gmail.com>
2023-07-02 17:36:09 +0200

    New ToDo from GPT

---
+++ Section_JS/Module_3-4_DOM/js/toDoFromGPT.js
@@ -0,0 +1,52 @@
+// Функция для генерации случайного цвета в формате HEX
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const input = document.getElementById("inputTask");
+const btnAdd = document.getElementById("btnAdd");
+const taskList = document.getElementById("taskList");
+
+input.addEventListener("keypress", onKeypress);
+btnAdd.addEventListener("click", onClick);
+
+function chechInputLenght() {
+  return input.value.length;
+}
+
+function onKeypress() {
+  if (chechInputLenght() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+function onClick() {
+  if (chechInputLenght() > 0) {
+    createListElement();
+  }
+}
+
+function createListElement() {
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  li.style.backgroundColor = getRandomColor();
+
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "Del";
+  btnDelete.addEventListener("click", delElement);
+  btnDelete.className="btn btn-success"
+  
+
+  li.appendChild(btnDelete);
+  taskList.appendChild(li);
+  input.value = "";
+}
+
+function delElement() {
+  this.parentNode.remove();
+}
+console.log(taskList);
