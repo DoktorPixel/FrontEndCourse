e58c4e59c5c4604d6f2c041c89bccfd9280d252d
unknown <miamby111@gmail.com>
2023-07-03 18:55:23 +0200

    changing todo list

--- Section_JS/Module_3-4_DOM/js/toDoFromGPT.js
+++ Section_JS/Module_3-4_DOM/js/toDoFromGPT.js
@@ -38,8 +38,7 @@ function createListElement() {
   const btnDelete = document.createElement("button");
   btnDelete.textContent = "Del";
   btnDelete.addEventListener("click", delElement);
-  btnDelete.className="btn btn-success"
-  
+  btnDelete.className = "btn btn-success";
 
   li.appendChild(btnDelete);
   taskList.appendChild(li);
@@ -49,4 +48,29 @@ function createListElement() {
 function delElement() {
   this.parentNode.remove();
 }
-console.log(taskList);
+
+// Создание кнопки
+const wrapper = document.querySelector(".wrapper");
+
+function createDiv() {
+  const wrapperForBtn = document.createElement("div");
+  wrapperForBtn.classList.add("wrapperBtn");
+  wrapper.appendChild(wrapperForBtn);
+  const buttonNew = document.createElement("button");
+  buttonNew.textContent = "Нажми меня";
+  wrapperForBtn.appendChild(buttonNew);
+
+  buttonNew.addEventListener("click", function () {
+    const image = document.createElement("img");
+    image.src =
+      "https://www.doubledtrailers.com/assets/images/random%20horse%20facts%20shareable.png";
+    wrapperForBtn.appendChild(image);
+
+    setTimeout(function () {
+      image.remove();
+    }, 1000);
+  });
+}
+createDiv();
+
+console.log(wrapper);
