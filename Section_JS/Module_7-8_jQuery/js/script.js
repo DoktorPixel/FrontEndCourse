cedaa022d4a521e0c397b565b9b45945dbfe1091
unknown <miamby111@gmail.com>
2023-07-04 17:55:31 +0200

    jQuery

---
+++ Section_JS/Module_7-8_jQuery/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	/* tabs */
+    $(".tabs__link").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("tabs__current");
+        $(this).parent().siblings().removeClass("tabs__current");
+        var tab = $(this).attr("href");
+        $(".tabs__content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+/* tooltips */
+//function show tooltip
+	var showTool = function showTooltip() {
+		name = $(this).attr('name');
+		$('label.fieldHint[for="'+name+'"]').fadeIn('slow');
+	};
+//function hide tooltip
+	var hideTool = function hideTooltip() {
+		var name = $(this).attr('name');
+		$('label.fieldHint[for="'+name+'"]').fadeOut('slow');
+	};
+
+//focus
+		$('#myForm input').focus(showTool)
+		.blur(hideTool);
+
+//hover
+		$('#myForm input').hover(showTool, hideTool);
+
+//btn
+		$('.btn').on('click', function(event) {
+			event.preventDefault();
+			$('.fieldHint').show();
+		});
+		
+});
+
